9b386fb8bc8266262d1cd9a416767fa005dfe077
Marco Moauro <marco.moauro@translated.net>
2024-05-29 07:27:19 +0200

    improvement

--- src/models/Leaderboard.js
+++ src/models/Leaderboard.js
@@ -8,36 +8,36 @@ export class Leaderboard {
   static getByScore = async (limit) => {
     log.info('Model::Leaderboard::getByScore', { limit })
 
-    const leaderboard = await this.computeLeaderboard(limit);
+    const leaderboard = await this.#computeLeaderboard(limit);
 
     return leaderboard
   }
 
-  static computeLeaderboard = async (limit) => {
+  static #computeLeaderboard = async (limit) => {
     log.info('Model::Leaderboard::computeLeaderboard', { limit })
 
-    const raw_leaderboard = await this._getRawLeaderboard(limit);
+    const raw_leaderboard = await this.#getRawLeaderboard(limit);
 
     const leaderboard = [];
-    this._parseResult(raw_leaderboard, leaderboard);
+    this.#parseResult(raw_leaderboard, leaderboard);
 
     const user_ids = leaderboard.map((entry) => entry.user_id);
-    const id_to_user_map = await this._getUserMap(user_ids)
+    const id_to_user_map = await this.#getUserMap(user_ids)
 
-    this._computeLeaderboardWithUserNames(leaderboard, id_to_user_map)
+    this.#computeLeaderboardWithUserNames(leaderboard, id_to_user_map)
 
     return leaderboard
   }
 
   // get the raw leaderboard from the cache
-  static _getRawLeaderboard = async (limit) => {
+  static #getRawLeaderboard = async (limit) => {
     const raw_leaderboard = await Cache.getClient().zrevrange(this.#cache_key, 0, limit - 1, 'WITHSCORES');
 
     return raw_leaderboard
   }
 
   // parse the result into an array of objects
-  static _parseResult = (raw_leaderboard, leaderboard) => {
+  static #parseResult = (raw_leaderboard, leaderboard) => {
     for (let i = 0; i < raw_leaderboard.length; i += 2) {
       leaderboard.push({
         user_id: parseInt(raw_leaderboard[i]),
@@ -47,7 +47,7 @@ export class Leaderboard {
   }
 
   // crete a map of user_id to user object
-  static _getUserMap = async (user_ids) => {
+  static #getUserMap = async (user_ids) => {
     const users = await User.list(user_ids);
     const id_to_user_map = users.reduce((acc, user) => {
       acc[user.id] = user;
@@ -58,7 +58,7 @@ export class Leaderboard {
   }
 
   // replace user_id with user name
-  static _computeLeaderboardWithUserNames = (leaderboard, id_to_user_map) => {
+  static #computeLeaderboardWithUserNames = (leaderboard, id_to_user_map) => {
     leaderboard.forEach((entry, index) => {
       const user_id = entry.user_id
 
